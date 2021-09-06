a9237ffe09b6a78fdb9b0ce11df413d2b06d4967
王文明 <wangwenming1985@gmail.com>
2021-09-06 19:14:33 +0800

    Puppeteer Node 服务 1.0.0

---
+++ Puppeteer/html2image.js
@@ -0,0 +1,62 @@
+const puppeteer = require('puppeteer');
+const { v4: uuidv4 } = require('uuid');
+
+// https://stackoverflow.com/questions/51529332/puppeteer-scroll-down-until-you-cant-anymore
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
+
+async function html2image(url, output) {
+  // 打开浏览器
+  const browser = await puppeteer.launch();
+  // 新建标签页
+  const page = await browser.newPage();
+  // 跳转指定页面
+  await page.goto(url);
+  // 设置页面大小，宽度为手机的 750px，高度无所谓
+  await page.setViewport({
+    width: 750,
+    height: 3000,
+    deviceScaleFactor: 1,
+  });
+
+  await autoScroll(page);
+
+  //await page.goto('https://m.baidu.com/'); // 跳转指定页面
+  // 等待页面渲染
+  // await page.waitForTimeout(9000);
+
+  // 截图
+  let filename = `${uuidv4()}.png`;
+  let path = `${output}/${filename}`;
+  await page.screenshot({
+    path,
+    fullPage: true,
+    captureBeyondViewport: true 
+  });
+
+  await browser.close();
+
+  return filename;
+}
+
+// 测试 
+// let url = 'https://mp.weixin.qq.com/s/S0kHvkH6nlkILnjovVSEaQ';
+// html2image(url, 'public/img');
+
+module.exports = html2image
---
+++ Puppeteer/index.js
@@ -0,0 +1,49 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+const serve = require('koa-static')
+const html2image = require('./html2image');
+const path = require('path');
+
+let port = 3008;
+const staticRoot = 'public'
+const imgPath = 'img'
+
+const app = new Koa();
+const router = new Router({
+    prefix: '/api',
+});
+
+router.get('/html2image', async (ctx, next) => {
+    let url = ctx.request.query.url;
+    let filename = await html2image(url, `${staticRoot}/${imgPath}`);
+    console.log(path);
+    ctx.body = {
+        code: 0,
+        msg: 'OK',
+        data: {
+            imgUrl: `${ctx.request.origin}/${imgPath}/${filename}`
+        }
+    };
+});
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}`);
+    await next();
+});
+
+app.use(serve(path.join(__dirname, 'public'), {
+    gzip: false
+}));
+
+// 加载路由中间件
+app.use(router.routes())
+   .use(router.allowedMethods())
+
+// 默认情况下，将所有错误输出到 stderr，除非 app.silent 为 true。 
+app.on('error', err => {
+    console.error('server error', err)
+});
+
+app.listen(port);
+console.log(`服务已在端口 ${port} 启动`);
