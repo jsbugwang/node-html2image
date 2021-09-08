a31574046b87d395990bb5707e2cb43574fb04ed
王文明 <wangwenming1985@gmail.com>
2021-09-08 19:31:59 +0800

    添加 --port 端口 必填命令行参数

--- Puppeteer/index.js
+++ Puppeteer/index.js
@@ -3,8 +3,21 @@ const Router = require('@koa/router');
 const serve = require('koa-static')
 const html2image = require('./html2image');
 const path = require('path');
+const argv = require('yargs').argv;
 
-let port = 3008;
+if (!argv.port) {
+    console.error(`命令行必须带上端口参数：--port 端口号`);
+    process.exit(1);
+}
+if (typeof argv.port !== 'number') {
+    console.error(`端口号必须是数字，${argv.port} 不符合`);
+    process.exit(2);
+}
+if (argv.port < 0 || argv.port > 65535) {
+    console.error(`端口号 ${argv.port} 不在范围[1, 65535]`);
+    process.exit(3);
+}
+
 const staticRoot = 'public'
 const imgPath = 'img'
 
@@ -45,5 +58,5 @@ app.on('error', err => {
     console.error('server error', err)
 });
 
-app.listen(port);
-console.log(`服务已在端口 ${port} 启动`);
+app.listen(argv.port);
+console.log(`服务已在端口 ${argv.port} 启动`);
